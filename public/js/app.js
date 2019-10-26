60d04a4b3a23d95e1edf09221f0ffdfd63e73811
vikram <apte.vikram@capgemini.com>
2019-10-26 23:40:53 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+let messageOne = document.querySelector('#message-1');
+let messageTwo = document.querySelector('#message-2');
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const location = search.value;
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+    
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            // console.log(data.error);
+            messageOne.textContent = data.error;
+            messageTwo.textContent = '';
+        } else {
+            console.log(data.location);
+            console.log(data.forecastData);
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecastData;
+        }
+    })
+});
+    
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/51a0af2388b5090beff711ec70ea8ba0/' + latitude + ',' + longitude
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
