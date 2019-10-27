002f1e4c8b3686227f3c4e573347e88a5a96685b
vikram <apte.vikram@capgemini.com>
2019-10-27 23:08:55 +0530

    Added high and low temprature details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, `${body.daily.data[0].summary} High temprature is ${body.daily.data[0].temperatureHigh} degress and low ${body.daily.data[0].temperatureLow} degress. It is currently ${body.currently.temperature} degress out. There is a ${body.currently.precipProbability}% chance of rain.`);
         }
     })
 }
